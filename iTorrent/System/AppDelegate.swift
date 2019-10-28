dba495a41fd263807306619efd2e73dfece9f8f9
a.nesterov <a.nesterov@gettransfer.com>
2019-10-28 15:35:42 +0300

    upd

--- iTorrent/System/AppDelegate.swift
+++ iTorrent/System/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 		FirebaseApp.configure()
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+//        GADMobileAds.sharedInstance().start(completionHandler: nil)
 		
         Manager.InitManager()
         
